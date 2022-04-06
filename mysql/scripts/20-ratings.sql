59fa285fd79e827c1a38b0a8bf284d2a8f9147ec
Bright Zheng <bright.zheng@outlook.com>
2022-04-06 11:07:48 +0800

    fix the syntax error in mysql v8

--- mysql/scripts/20-ratings.sql
+++ mysql/scripts/20-ratings.sql
@@ -10,7 +10,6 @@ CREATE TABLE ratings (
     PRIMARY KEY (sku)
 ) ENGINE=InnoDB;
 
-
-GRANT ALL ON ratings.* TO 'ratings'@'%'
-IDENTIFIED BY 'iloveit';
-
+CREATE USER 'ratings'@'%' IDENTIFIED BY 'iloveit';
+GRANT ALL PRIVILEGES ON ratings.* TO 'ratings'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
