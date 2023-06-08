047f6e98c442c791580bbaf4775759bb317ba163
Bright Zheng <bright.zheng@outlook.com>
2023-06-08 20:09:24 +0800

    fix load instability issue

--- selenium/load.py
+++ selenium/load.py
@@ -29,8 +29,10 @@ def request_interceptor(request):
 def try_and_click(driver, by_type, by_path):
     try:
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((by_type, by_path))).click()
-    except:
-        print("there was a retry for: " + by_path)
+    except Exception as error:
+        # handle the exception
+        print("there was a retry for: " + by_path + " due to: " + type(error).__name__)
+        time.sleep(0.2)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((by_type, by_path))).click()
 
 chrome_options = Options()
@@ -53,24 +55,20 @@ try:
 
     driver.refresh()
 
-    try_and_click(driver, By.LINK_TEXT, "Login / Register")
-    try_and_click(driver, By.XPATH, "/html/body/div/div[1]/div[2]/div/div/div[2]/table[1]/tbody/tr[1]/td[2]/input")
-    driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div/div/div[2]/table[1]/tbody/tr[1]/td[2]/input").send_keys("stan")
+    try_and_click(driver, By.XPATH, "//a[contains(text(),\'Login / Register\')]")
+    try_and_click(driver, By.XPATH, "(//input[@type=\'text\'])[2]")
+    driver.find_element(By.XPATH,   "(//input[@type=\'text\'])[2]").send_keys("stan")
     try_and_click(driver, By.XPATH, "//input[@type=\'password\']")
-    driver.find_element(By.XPATH, "//input[@type=\'password\']").send_keys("bigbrain")
+    driver.find_element(By.XPATH,   "//input[@type=\'password\']").send_keys("bigbrain")
     try_and_click(driver, By.XPATH, "//button[contains(.,\'Login\')]")
+
     try_and_click(driver, By.XPATH, "//span[contains(.,\'Artificial Intelligence\')]")
     try_and_click(driver, By.XPATH, "//a[contains(text(),\'Ewooid\')]")
-    try_and_click(driver, By.ID, "vote-4")
-    try_and_click(driver, By.LINK_TEXT, "Stan")
-    try_and_click(driver, By.CSS_SELECTOR, ".ng-scope > button")
-    try_and_click(driver, By.LINK_TEXT, "Watson")
+    try_and_click(driver, By.ID,    "vote-4")
+    try_and_click(driver, By.XPATH, "//a[contains(text(),\'Watson\')]")
     try_and_click(driver, By.XPATH, "//button[contains(.,\'Add to cart\')]")
-    try_and_click(driver, By.XPATH, "//span[contains(.,\'Robot\')]")
-    try_and_click(driver, By.LINK_TEXT, "Cybernated Neutralization Android")
-    try_and_click(driver, By.ID, "vote-5")
-    try_and_click(driver, By.LINK_TEXT, "Cart")
-    # try_and_click(driver, By.XPATH, "//button[contains(.,\'Checkout\')]")
+    try_and_click(driver, By.XPATH, "//a[contains(@href, \'cart\')]")
+    try_and_click(driver, By.XPATH, "//button[contains(.,\'Checkout\')]")
 
 finally:
     driver.quit()
